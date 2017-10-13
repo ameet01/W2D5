965cc319cc35db008ecae6f7a7f2b7423a6dde29
App Academy Student <student@appacademy.io>
2017-10-13 11:37:56 -0700

    Completed Phase 1

--- HashMap & LRU Cache/lib/p01_int_set.rb
+++ HashMap & LRU Cache/lib/p01_int_set.rb
@@ -1,22 +1,25 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
+    raise "Out of bounds" if is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
-  end
-
-  def validate!(num)
+    num > @store.length || num < 0
   end
 end
 
@@ -27,18 +30,24 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].each do |el|
+      return true if el == num
+    end
+    false
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -55,18 +64,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self[num].include?(num)
+      self[num] << num
+      @count += 1
+    end
+    if @count >= num_buckets
+      resize!
+    end
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].each do |el|
+      return true if el == num
+    end
+    false
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +95,13 @@ class ResizingIntSet
   end
 
   def resize!
+    @count = 0
+    @old_set = @store.dup
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @old_set.each do |bucket|
+      bucket.each do |el|
+        insert(el)
+      end
+    end
   end
 end
